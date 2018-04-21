e8e3038d8cc6304bb4276f6171fb6bffbbf28e88
Thomas Feron <thomas.feron@redspline.com>
2018-04-21 17:21:43 +0200

    First commit (moved from Optinomic/apps)

---
+++ main.js
@@ -0,0 +1,73 @@
+/**
+ * @name Optinomic - AppCtrl
+ * ---------------------------------------
+ * Controller of the Optinomic-Application.
+ */
+app.controller('AppCtrl', function($scope, $filter, dataService, scopeDService) {
+
+    // Data-Sharing (do not remove)
+    $scope.d = scopeDService;
+
+    // -----------------------------------
+    // Main - Functions
+    // -----------------------------------
+
+    $scope.loadMainData = function() {
+        // -----------------------------------
+        // Get Data: d.dataMain
+        // -----------------------------------
+        $scope.d.haveData = false;
+        var dataPromiseMain = dataService.getMainAppData();
+        dataPromiseMain.then(function(data) {
+
+            // Save Data to $scope.d
+            $scope.d.dataMain = data;
+
+            // Run App-Functions:
+            $scope.getEntrys();
+
+            // Finishing: Console Info & Init = done.
+            console.log('Welcome, ', $scope.d.dataMain.apps.current.name, $scope.d);
+            $scope.d.init = true;
+        });
+    };
+    $scope.loadMainData();
+
+
+
+    // -----------------------------------
+    // Application - Init
+    // -----------------------------------
+
+    $scope.getEntrys = function() {
+        // Get Data
+
+        $scope.d.belegung = [];
+        var api_call = dataService.getPatientModuleAnnotations();
+
+        api_call.success(function(data) {
+            $scope.d.belegung = data;
+
+
+            $scope.d.belegung.current_stay = null;
+
+            data.alle.forEach(function(stay, my_stay_index) {
+                if (parseInt(stay.bel_selector.optinomic_fid) === parseInt($scope.d.dataMain.params.stay_id)) {
+                    $scope.d.belegung.selected = stay;
+                };
+            });
+
+
+            $scope.d.haveData = true;
+            console.log('(✓) getEntrys ', $scope.d.belegung);
+        });
+
+        api_call.error(function(data) {
+            console.log('(ERROR) getEntrys ', data);
+        });
+
+    };
+
+
+
+});
---
+++ readme.md
@@ -0,0 +1,15 @@
+
+## Polypoint Lookup
+
+Liest die aktuellen Belegungsdaten aus Polypoint und zeigt diese an.
+(8-ung: Südhang only)
+
+
+###Anschrift
+*Optinomic GmbH*   
+*Haldenstrasse 7*     
+*CH - 8942 Oberrieden*     
+*+41(0)44 508 26 76*    
+*info@optinomic.com*   
+*[www.optinomic.com](http://www.optinomic.com)*   
+
